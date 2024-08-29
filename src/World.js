f2948f086ff66805c51dfa7396504dfcc63bdbf5
Marioood <121731808+Marioood@users.noreply.github.com>
2024-08-28 21:12:44 -0500

    desert generation and save disabling

--- src/World.js
+++ src/World.js
@@ -21,6 +21,7 @@ class World {
             "forest",
             "hill",
             "bop",
+            "desert",
             "void"
         ];
 
@@ -55,11 +56,13 @@ class World {
         }
 
         // save world on interval
-        setInterval(() => {
+		if(config.self.world.saveWorld) { // good for debugging
+			setInterval(() => {
 
-            this.save();
+				this.save();
 
-        }, config.self.world.saveInterval * 1000);
+			}, config.self.world.saveInterval * 1000);
+		}
     }
 
     setBlock(type, stream, bx, by, bz) {
@@ -141,7 +144,10 @@ class World {
             case "bop":
                 this.generateBop();
                 break;
-
+				
+            case "desert":
+                this.generateDesert();
+                break;
         }
 
         utils.log(`Generating done, took ${Math.round(performance.now() - start)}ms`);
@@ -387,6 +393,104 @@ class World {
                 }
             }
             
+    }
+	
+	generateDesert() {
+		const noise = perlin.generatePerlinNoise(this.z, this.x, {octaveCount: 3});
+		const shift = perlin.generatePerlinNoise(this.z, this.x, {octaveCount: 6});
+		const junk = perlin.generatePerlinNoise(this.z, this.x, {octaveCount: 4});
+		const water = perlin.generatePerlinNoise(this.z, this.x, {octaveCount: 5});
+		
+        const calculateHeight = (x, z) => {
+			let xOff = Math.floor(x * (shift[z + (x * this.z)] * 0.1 + 0.2));
+			let height = Math.sin(noise[z + (xOff * this.z)]) * 8;
+			height *= height;
+			height /= 6;
+			height += shift[z + (x * this.z)] * 16;
+            return Math.round(height) + this.halfY;
+        }
+		
+		const cactus = (x, y, z) => {
+			let height = Math.round(Math.random() * 4);
+			
+			for(let i = 0; i < height; i++) {
+				this.setBlock(24, false, x, y + i, z)
+			}
+		}
+		
+		const doOre = (x, height, z) => {
+			let ore;
+			if(Math.random() < 0.75) {
+				ore = 14; // gold
+			} else if (Math.random() < 0.1) {
+				ore = 16; // coal
+			} else {
+				ore = 15; // iron
+			}
+			
+			this.setBlock(ore, false, x, Math.floor(Math.random() * height) + 1, z);
+		}
+		
+		for(let z = 0; z < this.z; z++) {
+			for(let x = 0; x < this.x; x++) {
+				let height = calculateHeight(x, z);
+				let waterVal = water[z + (x * this.z)]
+				if(waterVal < 0.7) {
+					//dunes
+					for(let y = 1; y < height; y++) {
+						if(y > height - 6) {
+							this.setBlock(12, false, x, y, z);// ssand
+						} else {
+							this.setBlock(1, false, x, y, z); // stone
+						}
+					}
+					//junk
+					let junkVal = junk[z + (x * this.z)];
+					
+					if((Math.random() * 0.75) * junkVal > 0.5) {	
+						cactus(x, height, z);
+					} else if(Math.random() > 0.995) {
+						let junkBlock = 6; //sapling
+
+						if(junkVal > 0.75) {
+							junkBlock = 40; //red mushroom
+						} else if (junkVal < 0.25) {
+							junkBlock = 39; //brown mushrom
+						}
+						this.setBlock(junkBlock, false, x, height, z);
+					}
+					
+				//water
+				} else {
+					height = Math.round(-waterVal * 32) + this.halfY;
+					for(let y = 1; y <= this.halfY; y++) {
+						if(height > y) {
+							if(height - 3 > y) {
+								this.setBlock(1, false, x, y, z); // stone
+							} else {
+								this.setBlock(13, false, x, y, z); // gravel
+							}
+						} else {
+							this.setBlock(9, false, x, y, z); // water
+						}
+					}
+					
+					if(Math.random() * (waterVal * waterVal) > 0.7) {
+						this.setBlock(38, false, x, this.halfY + 1, z); // roses
+					}
+					
+					if(Math.random() > 0.9995) {
+						this.setBlock(41, false, x, height, z); //gold
+					}
+					
+				}
+				
+				doOre(x, height - 8, z);
+				doOre(x, height - 8, z);
+				doOre(x, height - 4, z);
+				this.setBlock(11, false, x, 0, z);
+			}
+		}
     }
 
     tree(tx, ty, tz, stream = false) {
