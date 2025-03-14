e05b948dab09fd2c7a171870a7226467e9c5f1e7
dottych <96998759+dottych@users.noreply.github.com>
2025-03-14 18:08:39 +0100

    git pain 1

--- src/Packets/ClientPacket.js
+++
@@ -1,9 +0,0 @@
-class ClientPacket {
-    constructor(client, buffer, type) {
-        this.client = client;
-        this.buffer = buffer;
-        this.type = type;
-    }
-}
-
-module.exports = ClientPacket;
